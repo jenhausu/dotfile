3575da0d9bc738c8cf1737132d9564982747ec18
蘇健豪 <jenhausu@osensetech.com>
2020-05-30 22:47:48 +0800

    setup fish plugin

--- installFish.bash
+++ installFish.bash
@@ -1,2 +1,20 @@
+# install Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+# install fish
 brew install fish
+
+# install oh-my-fish
+curl -L https://get.oh-my.fish | fish
+omf install xcode
+omf install gi
+omf install z
+omf install bang-bang
+brew install jq
+omf install weather
+
+# fisher
+curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
+brew install terminal-notifier
+fisher add franciscolourenco/done
+fisher add markcial/upto
