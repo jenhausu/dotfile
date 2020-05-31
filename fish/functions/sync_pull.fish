8cc27c50371e2da8de7cc5091043242cb9b92f77
蘇健豪 <jenhausu@osensetech.com>
2020-05-31 16:35:02 +0800

    fix[fish]: sync_pull func

--- fish/functions/sync_pull.fish
+++ fish/functions/sync_pull.fish
@@ -3,7 +3,7 @@ function sync_pull
 	cp ~/Documents/repository/dotfile/fish/config.fish ~/.config/fish/config.fish
 
 	# sync fish function
-	cp ~/Documents/repository/dotfile/fish/functions/ ~/.config/fish/functions/*.fish
+	cp ~/Documents/repository/dotfile/fish/functions/* ~/.config/fish/functions/
 
 	# sync git config
 	cp ~/Documents/repository/dotfile/.gitconfig ~/.gitconfig
