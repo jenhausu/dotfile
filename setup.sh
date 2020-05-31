81addec1d8f8a4d4e1ee0d1afc63fc9ce91eecf3
蘇健豪 <jenhausu@icloud.com>
2020-05-31 16:03:36 +0800

    fix: add missing syntax

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
-brewSetup.sh
-fishSetup.sh
+bash brewSetup.sh
+bash fishSetup.sh
 
 cp ~/Documents/repository/dotfile/fish/functions/sync_pull.fish ~/.config/fish/functions/sync_pull.fish
 cp ~/Documents/repository/dotfile/fish/functions/reload.fish ~/.config/fish/functions/reload.fish
