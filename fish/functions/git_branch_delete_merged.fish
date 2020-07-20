efbf7962b6d04374d9946a36c106ccb2293b99b1
蘇健豪 <jenhausu@osensetech.com>
2020-07-20 12:14:18 +0800

    chore: change indent

--- fish/functions/git_branch_delete_merged.fish
+++ fish/functions/git_branch_delete_merged.fish
@@ -1,22 +1,22 @@
 function git_branch_delete_merged
 	echo git fetch...
-    git fetch -p
+	git fetch -p
 
-    # get remote merged branch
-    set remote_merged_branchs (git branch --remote --merged origin/master | grep -v 'master' | cut -b 10- )
+	# get remote merged branch
+	set remote_merged_branchs (git branch --remote --merged origin/master | grep -v 'master' | cut -b 10- )
 
 	# if current branch is at any remote merged branch, checkout to master
-    set branch_current (git branch | sed -n -e 's/^\* \(.*\)/\1/p')
-    if contains $branch_current $remote_merged_branchs
-        git checkout master
-    end
+	set branch_current (git branch | sed -n -e 's/^\* \(.*\)/\1/p')
+	if contains $branch_current $remote_merged_branchs
+		git checkout master
+	end
 
-    # remove remote merged branch
-    git branch --remote --merged origin/master | grep -v 'master' | cut -b 10- | xargs git push --delete origin
+	echo 'remove remote merged branch'
+	git branch --remote --merged origin/master | grep -v 'master' | cut -b 10- | xargs git push --delete origin
 
-	# remove local branch is merged remotely
-    echo $remote_merged_branchs | xargs git branch -D
+	echo "remove local branch is merged remotely"
+	echo $remote_merged_branchs | xargs git branch -D
 
-	# remove local merged branch
+	echo 'remove local merged branch'
 	git branch --merged | egrep -v "(^\*|master|develop)" | xargs git branch -d
 end
