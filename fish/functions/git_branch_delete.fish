982398fc5ba6d1a8ca96e414de48d3980ff0ab7c
蘇健豪 <jenhausu@osensetech.com>
2020-07-20 12:15:03 +0800

    feat: remove git fetch -p

--- fish/functions/git_branch_delete.fish
+++ fish/functions/git_branch_delete.fish
@@ -1,13 +1,12 @@
 function git_branch_delete -w "git branch -D" -d "hard delete branch on lodal and remote"
-    set branch_current (git branch | sed -n -e 's/^\* \(.*\)/\1/p')
+    # delete branch on local
+	set branch_current (git branch | sed -n -e 's/^\* \(.*\)/\1/p')
     if [ "$branch_current" = "$argv" ]
         git checkout master
     end
     git branch -D $argv
 
-	echo git fetch...
-    git fetch -p
-
+	# delete branch on remote
     set branch_remote (git branch --remote | grep $argv | cut -b 10- )
     if test -n "$branch_remote"
         git push origin --delete $argv
