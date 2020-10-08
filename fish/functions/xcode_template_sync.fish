0f156d5816f89386367ce1b3443c1a285fd99f55
蘇健豪 <jenhausu@osensetech.com>
2020-10-08 18:26:13 +0800

    fish: update xcode_template_sync

--- fish/functions/xcode_template_sync.fish
+++ fish/functions/xcode_template_sync.fish
@@ -1,11 +1,13 @@
 function xcode_template_sync
 	set TEMPLATE_PATH ~/Library/Developer/Xcode/Templates/File\ Templates/Clean\ Swift
+
 	cp $TEMPLATE_PATH/Router.xctemplate/*.swift $TEMPLATE_PATH/Scene.xctemplate/UIViewController/
 	cp $TEMPLATE_PATH/Router.xctemplate/*.swift $TEMPLATE_PATH/Scene.xctemplate/UITableViewController/
 	cp $TEMPLATE_PATH/Router.xctemplate/*.swift $TEMPLATE_PATH/Scene.xctemplate/UICollectionViewController/
 	cp $TEMPLATE_PATH/View\ Controller.xctemplate/UIViewController/*.swift $TEMPLATE_PATH/Scene.xctemplate/UIViewController/
 	cp $TEMPLATE_PATH/View\ Controller.xctemplate/UITableViewController/*.swift $TEMPLATE_PATH/Scene.xctemplate/UITableViewController/
 	cp $TEMPLATE_PATH/View\ Controller.xctemplate/UICollectionViewController/*.swift $TEMPLATE_PATH/Scene.xctemplate/UICollectionViewController/
+	cp $TEMPLATE_PATH/TableViewCell.xctemplate/*.swift $TEMPLATE_PATH/Scene.xctemplate/UITableViewController/
 	cp $TEMPLATE_PATH/Interactor.xctemplate/*.swift $TEMPLATE_PATH/Scene.xctemplate/UIViewController/
 	cp $TEMPLATE_PATH/Interactor.xctemplate/*.swift $TEMPLATE_PATH/Scene.xctemplate/UITableViewController/
 	cp $TEMPLATE_PATH/Interactor.xctemplate/*.swift $TEMPLATE_PATH/Scene.xctemplate/UICollectionViewController/
@@ -21,4 +23,6 @@ function xcode_template_sync
 	cp $TEMPLATE_PATH/Models.xctemplate/*.swift $TEMPLATE_PATH/Scene.xctemplate/UIViewController/
 	cp $TEMPLATE_PATH/Models.xctemplate/*.swift $TEMPLATE_PATH/Scene.xctemplate/UITableViewController/
 	cp $TEMPLATE_PATH/Models.xctemplate/*.swift $TEMPLATE_PATH/Scene.xctemplate/UICollectionViewController/
+
+	cp -r ~/Library/Developer/Xcode/Templates/File\ Templates/  ~/Documents/repository/XcodeTemplate-VIP/
 end
