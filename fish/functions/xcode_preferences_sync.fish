29020587687e43b2862242bcf16f9192524c2da7
蘇健豪 <jenhausu@osensetech.com>
2021-03-09 17:11:52 +0800

    feat[fish]: xcode_preferences_sync

---
+++ fish/functions/xcode_preferences_sync.fish
@@ -0,0 +1,12 @@
+function xcode_preferences_sync
+	if test $argv[1] = "push"
+		cp -R ~/Library/Developer/Xcode/UserData/CodeSnippets ~/Documents/repository/XcodePreferences/
+		cp -R ~/Library/Developer/Xcode/UserData/KeyBindings ~/Documents/repository/XcodePreferences/
+		cp ~/Library/Preferences/com.apple.dt.Xcode.plist ~/Documents/repository/XcodePreferences/
+	end
+
+	if test $argv[1] = "pull"
+		cp -R ~/Documents/repository/XcodePreferences/ ~/Library/Developer/Xcode/UserData/
+		cp ~/Documents/repository/XcodePreferences/com.apple.dt.Xcode.plist ~/Library/Preferences/
+	end
+end
