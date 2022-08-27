05178af73552c6a8cb3c49f428528de19154da03
蘇健豪 <oyster@sujianhaoOSENSE-deM1-Pro.local>
2022-08-27 11:48:27 +0800

    feat: update fish setup

--- fishSetup.sh
+++ fishSetup.sh
@@ -2,7 +2,7 @@
 brew install fish
 
 # install oh-my-fish
-curl -L https://get.oh-my.fish | fish
+curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
 omf install xcode
 omf install gi
 omf install z
@@ -15,6 +15,6 @@ omf install https://github.com/b4b4r07/enhancd
 # fisher
 curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
 brew install terminal-notifier
-fisher add franciscolourenco/done
-fisher add markcial/upto
-fisher add acomagu/fish-async-prompt
+fisher install franciscolourenco/done
+fisher install markcial/upto
+fisher install acomagu/fish-async-prompt
