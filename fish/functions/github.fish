5a3c9960f00ede1484bafca4613fd0204f2809de
蘇健豪 <jenhau.su@ichef.com.tw>
2024-02-27 10:59:50 +0800

    github: pull param

--- fish/functions/github.fish
+++ fish/functions/github.fish
@@ -6,6 +6,8 @@ function github -d "Open current repository github page."
 				sed 's/....$//')
 	if set -q argv[1] && test $argv[1] = action
 		set url $url/actions
+	else if set -q argv[1] && test $argv[1] = pr
+		set url $url/pulls
 	end
 	open -a Safari $url
 end
